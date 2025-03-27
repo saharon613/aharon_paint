8c87fee819fed7255f38d7e7ed207fbd062de34d
Shoshana Aharon <shoshaha@Shoshanas-Air-2.fios-router.home>
2025-03-27 15:28:48 -0400

    updated

---
+++ src/main/java/org/example/DrawingComponent.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class DrawingComponent extends JComponent {
+
+    private final BufferedImage image = new BufferedImage(
+            800,
+            600,
+            BufferedImage.TYPE_INT_RGB
+    );
+
+    private int oldx = -1;
+    private int oldy = -1;
+
+    public DrawingComponent() {
+        // set the image to be all white - white background
+        Graphics g = image.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, image.getWidth(), image.getHeight());
+    }
+    @Override
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);                // this clears the super one so that you can override it w/ your own
+
+        g.drawImage(image, 0, 0, null);
+
+//        g.setColor(Color.MAGENTA);
+//        g.drawLine(0, 0, getWidth(), getHeight());
+//        g.drawLine(getWidth(), 0, 0, getHeight());
+    }
+
+    public void drawFromMouse(int x, int y) {
+        Graphics g = image.getGraphics();
+        g.setColor(Color.MAGENTA);
+        if (oldx != -1 && oldy != -1) {
+            g.drawLine(oldx, oldy, x, y);
+        }
+        oldx = x;
+        oldy = y;
+
+        repaint();
+    }
+
+    public void newClick() {
+        oldx = -1;
+        oldy = -1;
+        repaint();
+    }
+}
---
+++ src/main/java/org/example/PaintGui.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import javax.swing.*;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class PaintGui extends JFrame {
+
+    private final DrawingComponent canvas = new DrawingComponent();
+    private final JButton paintButton = new JButton("Paint");
+    private final JColorChooser colorChooser = new JColorChooser();
+    private final JButton lineButton = new JButton("Line");
+
+    public PaintGui() {
+        setTitle("Paint");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        AbstractColorChooserPanel[] panels = { colorChooser.getChooserPanels()[0] };
+        colorChooser.setChooserPanels(panels);
+
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(new BorderLayout());
+        northPanel.setPreferredSize(new Dimension(getWidth(), 110));
+        colorChooser.setPreferredSize(new Dimension(70, 110));
+        colorChooser.setPreviewPanel(new JPanel());
+
+        northPanel.add(paintButton, BorderLayout.WEST);
+        northPanel.add(colorChooser, BorderLayout.CENTER);
+        northPanel.add(lineButton, BorderLayout.EAST);
+        add(northPanel, BorderLayout.NORTH);
+        add(canvas, BorderLayout.CENTER);
+
+        canvas.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent event) {        // where the mouse is currently within the component
+                canvas.drawFromMouse(event.getX(), event.getY());
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+
+        canvas.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent event) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent event) {
+                canvas.newClick();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        PaintGui frame = new PaintGui();
+        frame.setVisible(true);
+    }
+}
