e95da868199b88c76a08bce64caa1277c038b585
Shoshana Aharon <shoshaha@Shoshanas-MacBook-Air-2.local>
2025-04-30 14:10:25 -0400

    updated

--- src/main/java/aharon/paint/BucketFillTool.java
+++ src/main/java/aharon/paint/BucketFillTool.java
@@ -11,35 +11,48 @@ public class BucketFillTool implements Tool {
         int pixelColor = image.getRGB(x, y);
         int newColor = fillColor.getRGB();
 
-        if (pixelColor == newColor) { return; }
+        if (pixelColor == newColor) {
+            return;
+        }
 
-        floodFillRecursive(image, x, y, pixelColor, newColor);
+        bucketFill(image, x, y, pixelColor, newColor);
         g.drawImage(image, 0, 0, null);
     }
 
-    private void floodFillRecursive(BufferedImage image, int x, int y, int pixelColor, int newColor) {
+    private void bucketFill(BufferedImage image, int x, int y, int pixelColor, int newColor) {
         int width = image.getWidth();
         int height = image.getHeight();
 
-        if (x < 0 || x >= width || y < 0 || y >= height) { return; }
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            return;
+        }
 
-        if (image.getRGB(x, y) != pixelColor) { return; }
+        if (image.getRGB(x, y) != pixelColor) {
+            return;
+        }
 
         image.setRGB(x, y, newColor);
 
-        floodFillRecursive(image, x + 1, y, pixelColor, newColor); // Right
-        floodFillRecursive(image, x - 1, y, pixelColor, newColor); // Left
-        floodFillRecursive(image, x, y + 1, pixelColor, newColor); // Down
-        floodFillRecursive(image, x, y - 1, pixelColor, newColor); // Up
+        bucketFill(image, x + 1, y, pixelColor, newColor); // Right
+        bucketFill(image, x - 1, y, pixelColor, newColor); // Left
+        bucketFill(image, x, y + 1, pixelColor, newColor); // Down
+        bucketFill(image, x, y - 1, pixelColor, newColor); // Up
     }
 
-    @Override public void dragged(Graphics2D g, int x, int y) {}
+    @Override
+    public void dragged(Graphics2D g, int x, int y) {
+    }
 
-    @Override public void preview(Graphics2D g) {}
+    @Override
+    public void preview(Graphics2D g) {
+    }
 
-    @Override public void released(Graphics2D g, int x, int y) {}
+    @Override
+    public void released(Graphics2D g, int x, int y) {
+    }
 
-    @Override public void setColor(Color color) {
+    @Override
+    public void setColor(Color color) {
         this.fillColor = color;
     }
 }
